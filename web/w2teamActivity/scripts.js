cfd03a0f97d7b0e34ee5bc250bdf05f7fc5b0c81
soldochris <soldevilla.christian@gmail.com>
2021-01-12 01:40:20 -0500

    stretch challenges done

--- web/w2teamActivity/scripts.js
+++ web/w2teamActivity/scripts.js
@@ -1,18 +1,34 @@
-//getting elements from the DOM
-const clickmeBtn = document.getElementById("clickmeBtn");
-const changeColorBtn = document.getElementById("changeColorBtn");
-const userColor = document.getElementById("userColor");
-const firstDiv = document.getElementById("firstDiv");
+// //getting elements from the DOM
+// const clickmeBtn = document.getElementById("clickmeBtn");
+// const changeColorBtn = document.getElementById("changeColorBtn");
+// const userColor = document.getElementById("userColor");
+// const firstDiv = document.getElementById("firstDiv");
 
-//events Listeners
-clickmeBtn.addEventListener("click",displayMessage);
-changeColorBtn.addEventListener("click",changeColor);
+// //events Listeners
+// clickmeBtn.addEventListener("click",displayMessage);
+// changeColorBtn.addEventListener("click",changeColor);
 
-//declaring functions
+// //declaring functions
+// function displayMessage(){
+//   alert("Clicked!");
+// }
+
+// function changeColor(){
+//   firstDiv.style.backgroundColor = userColor.value;
+// }
+
+
+$("#clickmeBtn").click(displayMessage);
 function displayMessage(){
   alert("Clicked!");
 }
 
+$("#changeColorBtn").click(changeColor);
 function changeColor(){
-  firstDiv.style.backgroundColor = userColor.value;
+  $("#firstDiv").css("background-color", $("#userColor").val());
+}
+
+$("#changeVisibilityBtn").click(changeVisibility);
+function changeVisibility(){
+  $("#thirdDiv").fadeToggle("slow", "linear");
 }
